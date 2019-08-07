187d692ae3fc2941dec862a25f52b88e61532a42
MGidi <53842052+MGidi@users.noreply.github.com>
2019-08-07 03:36:30 +0100

    Add files via upload

---
+++ One_Line_Consume_parameters_from_OCUM_on_a_PowerShell_script.ps1
@@ -0,0 +1,5 @@
+﻿#create hashtable, Get rid of chars that break the script, append any word that start with " -" to a hashtable index with the input following it until the next " -" as a value.
+$parms=@{};" "+($args -replace "\]|\[|@"," " -join " ") -split " -"|?{$_}|%{$t=$_ -split " ",2;$parms.($t[0])=$t[1]}
+
+#run on $parms.eventArgs and append the first word before a "=" as a eventArgs_* index, and as the value any words after it  - until the next word that come before "=".
+($parms.eventArgs -split " "|%{if($_ -match "="){".cut."}$_}) -join " " -split ".cut. "|?{$_}|%{$t=$_ -split "=";$parms.("eventArgs_"+$t[0])=$t[1]}  
